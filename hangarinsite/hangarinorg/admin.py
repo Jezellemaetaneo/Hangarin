3b11216a8965c1866a32e6c2678fcbe001e3fcce
Jezellemaetaneo <202380324@psu.palawan.edu.ph>
2025-10-15 00:15:58 +0800

    Im tired

--- hangarinsite/hangarinorg/admin.py
+++ hangarinsite/hangarinorg/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Task, Substask, Category, Priority, Note
+
+admin.site.register(Task)
+admin.site.register(Substask)
+admin.site.register(Category)
+admin.site.register(Priority)
+admin.site.register(Note)
